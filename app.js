6455ed3ad953fb53904cad7ad3436df2da785037
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-09-10 22:01:36 +0900

    [add/update] change the structure of the project with an MVC Pattern.

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+// express라는 이름의 파일을 찾아보고, 없으면 node_modules내부에서 찾아본다.
+// const express = require("express");
+const app = express();
+
+app.set("view engine", "pug");
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+// app을 import할때 위에서 만든 app object를 넘겨준다.
+export default app;
