19e39f1f07926cbc839d079621f95cff1aefa1e8
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-09-11 22:54:16 +0900

    [add/update] change template structure using partials with pug and update logic in controller.

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 // express라는 이름의 파일을 찾아보고, 없으면 node_modules내부에서 찾아본다.
 // const express = require("express");
 const app = express();
 
-app.set("view engine", "pug");
-
-app.use(morgan("dev"));
 app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
+app.use(morgan("dev"));
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
