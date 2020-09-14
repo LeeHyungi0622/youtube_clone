2959f76e4e65bcafb75cd4fbe5ddc1bf685a6fac
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-09-14 20:26:51 +0900

    [add/update] update template and logic in controller.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,15 +1,27 @@
+import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) =>
-    res.render("home", { pageTitle: "Home", potato: 1234 });
+    res.render("home", { pageTitle: "Home", videos });
 
 export const search = (req, res) => {
     const {
         query: { term: searchingBy }
     } = req;
-    res.render("search", { pageTitle: "Search", searchingBy });
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
     res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description }
+    } = req;
+    // To Do: Upload and save video (video id)
+    res.redirect(routes.videoDetail(324393));
+};
+
 export const videoDetail = (req, res) =>
     res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
