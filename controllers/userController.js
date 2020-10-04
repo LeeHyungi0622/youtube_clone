291cc78ae907b42c5dc19d9843651abdee3a05ce
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-10-04 15:11:16 +0900

    [update] update Facebook Authenticate function.

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,6 +76,38 @@ export const postGithubLogin = (req, res) => {
     res.redirect(routes.home);
 };
 
+export const facebookLogin = passport.authenticate("facebook");
+
+export const facebookLoginCallback = async(_, __, profile, cb) => {
+    const {
+        _json: { id, name, email }
+    } = profile;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            user.facebookId = id;
+            user.avatarUrl = `https://graph.facebook.com/${id}/picture?type=large`;
+            user.name = name;
+            user.save();
+            // user정보만 passport에 넘겨줘서 해당 user정보를 cookie에 저장해준다.
+            return cb(null, user);
+        }
+        const newUser = await User.create({
+            email,
+            name,
+            facebookId: id,
+            avatarUrl: `https://graph.facebook.com/${id}/picture?type=large`
+        });
+        return cb(null, newUser);
+    } catch (error) {
+        return cb(error);
+    }
+};
+
+export const postFacebookLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => {
     req.logout();
     //To Do: Process Log Out
@@ -88,6 +120,16 @@ export const getMe = (req, res) => {
     res.render("userDetail", { pageTitle: "User Detail", user: req.user });
 };
 
-export const userDetail = (req, res) => res.render("userDetail");
+export const userDetail = async(req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        const user = await User.findById(id);
+        res.render("userDetail", { pageTitle: "User Detail", user });
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+};
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
--- passport.js
+++ passport.js
@@ -1,9 +1,13 @@
 import passport from "passport";
 import dotenv from "dotenv";
 import GithubStrategy from "passport-github";
+import FacebookStrategy from "passport-facebook";
 import User from "./models/User";
 import routes from "./routes";
-import { githubLoginCallback } from "./controllers/userController";
+import {
+    githubLoginCallback,
+    facebookLoginCallback
+} from "./controllers/userController";
 
 dotenv.config();
 
@@ -18,6 +22,18 @@ passport.use(
             callbackURL: `http://localhost:4000${routes.githubCallback}`
         }, // Github page에서 되돌아왔을때의 처리
         githubLoginCallback
+    )
+);
+
+passport.use(
+    new FacebookStrategy({
+            clientID: process.env.FB_ID,
+            clientSecret: process.env.FB_SECRET,
+            callbackURL: `https://fluffy-ladybug-25.serverless.social${routes.facebookCallback}`,
+            profileFields: ["id", "displayName", "photos", "email"],
+            scope: ["public_profile", "email"]
+        },
+        facebookLoginCallback
     )
 );
 
